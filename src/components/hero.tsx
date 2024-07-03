01539803985813162ee119c5826e5dabfd1eecf8
manishmh <manishmh982@gmail.com>
2024-07-03 20:26:26 +0530

    sonner added for messages dispalys

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,14 +1,27 @@
+'use client'
+
 import Image from "next/image";
 import React from "react";
+import { RiArrowRightSFill } from "react-icons/ri";
+import { toast } from "sonner";
 
 const Hero = () => {
+  const handleNotImplemented = () => {
+    toast.error("Feature in progress");
+  };
+
   return (
     <div className="flex flex-col justify-between h-full max-w-screen-2xl mx-auto pb-8">
       <div className="flex h-full flex-col items-center justify-center">
-        <div className="relative grid place-items-center gap-8 rounded-full">
-          <div className="text-5xl md:text-7xl xl:text-9xl font-medium font-serif">HueHarmony</div>
-          <div className="uppercase border font-medium rounded-full border-black max-w-sm w-full text-center py-2 text-sm 2xl:text-base">
-            Your skin your vibe
+        <div className="relative grid place-items-center gap-8 rounded-full cursor-default">
+          <div className="text-5xl md:text-7xl xl:text-9xl font-medium font-serif">
+            HueHarmony
+          </div>
+          <div className="group cursor-pointer uppercase flex gap-2 justify-center items-center border rounded-full border-black max-w-sm w-full text-center py-2 text-sm 2xl:text-base" onClick={handleNotImplemented}>
+            <span>CLONE YOUR FACE</span>
+            <div className="group-hover:translate-x-2 duration-500 transition-all">
+              <RiArrowRightSFill className="text-2xl" />
+            </div>
           </div>
           <Image
             src="/download.png"
@@ -21,7 +34,7 @@ const Hero = () => {
       </div>
       <div className="flex justify-between pb8">
         <div className="uppercase text-sm 2xl:text-base px-4">
-          www.hueharmanoy.com
+          Your skin your vibe
         </div>
         <div className="uppercase text-sm 2xl:text-base px-4">Azure Stars</div>
       </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,14 @@
+"use client";
 
 import { BsSearch } from "react-icons/bs";
 import { GrMenu } from "react-icons/gr";
+import { toast } from "sonner";
 
 const Navbar = () => {
+  const handleNotImplemented = () => {
+    toast.error("Feature in progress");
+  };
+
   return (
     <div className="flex justify-between max-w-screen-2xl mx-auto px-4 py-8 items-center w-full">
       <div className="uppercase text-lg 2xl:text-xl font-medium">MLSA</div>
@@ -10,9 +16,16 @@ const Navbar = () => {
         <div className="w-20 h-[2px] bg-black"></div>
         <div className="arrow-right"></div>
       </div>
-      <div className="text-xl 2xl:text-2xl flex gap-4 items-center">
-        <BsSearch className="text-lg 2xl:text-2xl"/>
-        <GrMenu />
+      <div
+        className="text-xl 2xl:text-2xl flex gap-4 items-center"
+        onClick={handleNotImplemented}
+      >
+        <div className="cursor-pointer">
+          <BsSearch className="text-lg 2xl:text-2xl" />
+        </div>
+        <div className="cursor-pointer">
+          <GrMenu />
+        </div>
       </div>
     </div>
   );
